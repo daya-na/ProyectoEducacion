89cd4e37d65221b30832a4120443eb191d58c529
daya-na <67805612+daya-na@users.noreply.github.com>
2021-11-25 07:06:29 -0500

    Actualizando Metricas_Educativas.py

--- Metricas_Educativas.py
+++ Metricas_Educativas.py
@@ -13,17 +13,17 @@ external_stylesheets = ["https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-# PROGRAMAS EDUCATIVOS POR CANTIDAD DE MATRICULADOS
+# ANALISIS 01: PROGRAMAS EDUCATIVOS POR CANTIDAD DE MATRICULADOS
 con = Connection()
 con.openConnection()
 query = pd.read_sql_query(sql.ProgramsByNumberRegister(), con.connection)
 con.closeConnection()
-dfCases = pd.DataFrame(query, columns=["nombre_prog", "cant_matriculados"])
+dfCases = pd.DataFrame(query, columns=[ "nombre_prog", "cant_matriculados"])
 
 # Tabla
 figTable = go.Figure()
-figTable.add_trace(go.Table(header=dict(
-            values=["nombre_prog", "cant_matriculados"],
+figTable.add_trace(go.Table(header=dict( 
+            values=[ "nombre_prog", "cant_matriculados"],
             font=dict(size=10)
                 ),
                 cells=dict(
@@ -33,8 +33,8 @@ figTable.add_trace(go.Table(header=dict(
                 )
             )
 
-# Grafico barras
-figBarCases = px.bar(dfCases.head(20), x="nombre_prog", y="cant_matriculados")
+# Grafico barras Vertical 
+figBarCases = px.bar(dfCases.head(10), y="nombre_prog", x="cant_matriculados", orientation='h')
 
 
 # ANALISIS 02: PROPORCIÓN DE INSTITUCIONES POR UBICACIÓN
@@ -44,23 +44,25 @@ query = pd.read_sql_query(sql.InstitutionsByLocation(), con.connection)
 con.closeConnection()
 dfInsUb = pd.DataFrame(query, columns=["nombre_dep", "nombre_mun", "cant_inst"])
 
-# Grafico barras
-figBarInsUb = px.bar(dfInsUb.head(20), x="nombre_dep", y="cant_inst", color= 'nombre_mun')
+# Grafico barras vertical
+figBarInsUb = px.bar(dfInsUb.head(20), y="nombre_dep", x="cant_inst", color= 'nombre_mun', orientation='h')
 
-# ANALISIS 03: CANTIDAD DE PROGRAMAS ACADÉMICOS BRINDADOS POR INSTITUCIONES
+# ANALISIS 03: CANTIDAD DE PROGRAMAS ACADÉMICOS BRINDADOS POR INSTITUCIONES SEGUN NIVEL ACADEMICO
 con = Connection()
 con.openConnection()
 query = pd.read_sql_query(sql.ProgramsByInstitution(), con.connection)
 con.closeConnection()
-dfPrgIns = pd.DataFrame(query, columns=["id_inst", "nombre_inst", "cant_programas"])
+#dfPrgIns = pd.DataFrame(query, columns=["nombre_inst", "nivel_academico", "cant_programas"])
+dfPrgIns = pd.DataFrame(query, columns=["nombre_inst", "nivel_academico", "cant_programas"])
 
-# Grafico barras
-figBardfPrgIns = px.bar(dfPrgIns.head(20), x="nombre_inst", y="cant_programas")
+# Grafico barras vertical
+figBardfPrgIns = px.bar(dfPrgIns.head(10), y="nombre_inst", x="cant_programas", orientation='h', color="nivel_academico")
+
 
 # Tabla
 figTablePrgIns = go.Figure()
 figTablePrgIns.add_trace(go.Table(header=dict(
-            values=["id_inst","nombre_inst", "cant_programas"],
+            values=["nombre_inst", "nivel_academico", "cant_programas"],
             font=dict(size=10)
                 ),
                 cells=dict(
@@ -78,9 +80,9 @@ query = pd.read_sql_query(sql.StudentsByRegistration(), con.connection)
 con.closeConnection()
 dfStuReg = pd.DataFrame(query, columns=["anio", "semestre", "cant_matriculados"])
 
-# Grafico barras
-#figBarfStuReg = px.bar(dfStuReg.head(20), x="anio", y="cant_matriculados")
-figBarfStuReg = px.bar(dfStuReg.head(20), x="anio", y="cant_matriculados", color= 'semestre')
+# Grafico de pie
+figBarfStuReg = px.pie(dfStuReg.head(12), names="anio", values="cant_matriculados")
+
 
 # Grafico lineas
 figLineStuReg = px.line(dfStuReg.head(20), x="anio", y="cant_matriculados", color='semestre', markers=True)
@@ -93,7 +95,7 @@ query = pd.read_sql_query(sql.MaxProgramsByInstitution(), con.connection)
 con.closeConnection()
 dfMaPr = pd.DataFrame(query, columns=["nombre_prog", "cant_inst"])
 
-# Grafico barras
+# Grafico barras horizontal
 figBarfMaPr = px.bar(dfMaPr.head(20), x="nombre_prog", y="cant_inst")
 
 # Grafico pie
@@ -101,48 +103,170 @@ figPieMaPr = px.pie(dfMaPr.head(5), names="nombre_prog", values="cant_inst")
 
 # Layout 
 app.layout = html.Div(children=[
-    html.H1(children='Dashboard Metricas educativas '),
-    html.H2(children='Analisis 01: Cantidad de matriculados por programas educativos'),
-    dcc.Graph(
-          id = 'tableProgramsByNumberRegister',
-          figure=figTable
-    ), 
-    dcc.Graph(
-        id='barProgramsByNumberRegister',
-        figure=figBarCases
-    ),
-    html.H2(children='Analisis 02: Proporción de instituciones por ubicación'),
-    dcc.Graph(
-          id = 'barInstitutionsByLocation',
-          figure=figBarInsUb
-    ),
-    html.H2(children='Analisis 03: Cantidad de programas académicos brindados por institución'),
-    dcc.Graph(
-        id='tableProgramsByInstitution',
-        figure=figTablePrgIns
-    ),
-    dcc.Graph(
-        id='barProgramsByInstitution',
-        figure=figBardfPrgIns
-    ),
-    html.H2(children='Analisis 04: Proporción de alumnos matriculados segun periodo de matricula'),
-    dcc.Graph(
-        id='barStudentsByRegistration',
-        figure=figBarfStuReg
-    ),
-    dcc.Graph(
-        id='lineStudentsByRegistration',
-        figure=figLineStuReg
-    ),
-    html.H2(children='Analisis 05 : Programas académicos con mayor presencia en instituciones'),
-    dcc.Graph(
-        id='barMaxProgramsByInstitution',
-        figure=figBarfMaPr
-    ),
-    dcc.Graph(
-        id='pieMaxProgramsByInstitution',
-        figure=figPieMaPr
-    )
+    html.H1(className ="container-fluid bg-primary text-white text-center p-4" ,children='Dashboard Metricas educativas '),
+    
+    #Container para el ancho completo 
+    html.Div(className="container-fluid", children=[
+        html.H3(className="p-3", children='Analisis 01: Cantidad de matriculados por programas educativos'),
+        # Filas para los casos 
+        html.Div(className="row", children=[
+            # columna para barras verticales 
+            html.Div(className="col-12 col-xl-6", children=[
+                html.Div(className="card", children=[
+                    html.Div(className="card-header", children=[
+                        html.H6(children='Tabla de programas academicos y cantidad de matriculados'),
+                        ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                  id = 'tableProgramsByNumberRegister',
+                                  figure=figTable
+                            ),
+                            ])
+                    ])
+                ]),
+            # columna para barras verticales 
+                html.Div(className="col-12 col-xl-6", children=[
+                    html.Div(className="card", children=[
+                        html.Div(className="card-header", children=[
+                            html.H6(children='Grafico de barras de programas academicos y cantidad de matriculados'),
+                            ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='barProgramsByNumberRegister',
+                                figure=figBarCases
+                            ),
+                            ])
+                        ])
+                    ])
+            ])
+        ]),
+     #Container para el ancho completo 
+    html.Div(className="container-fluid", children=[
+        html.H3(className="p-3",children='Analisis 02: Proporción de instituciones por ubicación'),
+        # Filas para los casos 
+        html.Div(className="row", children=[
+            # columna para barras verticales 
+            html.Div(className="col-12 col-xl-12", children=[
+                html.Div(className="card", children=[
+                    html.Div(className="card-header", children=[
+                        html.H6(children='Grafico de barras de Proporción de instituciones por ubicación'),
+                        ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                  id = 'barInstitutionsByLocation',
+                                  figure=figBarInsUb
+                            ),
+                            ])
+                    ])
+                ])
+            ])
+        ]),
+    #Container para el ancho completo 
+    html.Div(className="container-fluid", children=[
+        html.H3(className="p-3",children='Analisis 03: Cantidad de programas académicos brindados por institución según nivel academico'),
+        # Filas para los casos 
+        html.Div(className="row", children=[
+            # columna para barras verticales 
+            html.Div(className="col-12 col-xl-6", children=[
+                html.Div(className="card", children=[
+                    html.Div(className="card-header", children=[
+                        html.H6(children='Tabla de programas academicos y cantidad de matriculados'),
+                        ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='tableProgramsByInstitution',
+                                figure=figTablePrgIns
+                            ),
+                            ])
+                    ])
+                ]),
+            # columna para barras verticales 
+                html.Div(className="col-12 col-xl-6", children=[
+                    html.Div(className="card", children=[
+                        html.Div(className="card-header", children=[
+                            html.H6(children='Grafico de barras de programas academicos y cantidad de matriculados'),
+                            ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='barProgramsByInstitution',
+                                figure=figBardfPrgIns
+                            ),
+                            ])
+                        ])
+                    ])
+            ])
+        ]),
+    #Container para el ancho completo 
+    html.Div(className="container-fluid", children=[
+        html.H3(className="p-3",children='Analisis 04: Proporción de alumnos matriculados segun periodo de matricula'),
+        # Filas para los casos 
+        html.Div(className="row", children=[
+            # columna para barras verticales 
+            html.Div(className="col-12 col-xl-6", children=[
+                html.Div(className="card", children=[
+                    html.Div(className="card-header", children=[
+                        html.H6(children='Grafico de pie de programas academicos y cantidad de matriculados'),
+                        ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='barStudentsByRegistration',
+                                figure=figBarfStuReg
+                            ),
+                            ])
+                    ])
+                ]),
+            # columna para barras verticales 
+                html.Div(className="col-12 col-xl-6", children=[
+                    html.Div(className="card", children=[
+                        html.Div(className="card-header", children=[
+                            html.H6(children='Grafico de lineas de programas academicos y cantidad de matriculados'),
+                            ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='lineStudentsByRegistration',
+                                figure=figLineStuReg
+                            ),
+                            ])
+                        ])
+                    ])
+            ])
+        ]),
+      #Container para el ancho completo 
+    html.Div(className="container-fluid", children=[
+        html.H3(className="p-3",children='Analisis 05 : Programas académicos con mayor presencia en instituciones'),
+        # Filas para los casos 
+        html.Div(className="row", children=[
+            # columna para barras verticales 
+            html.Div(className="col-12 col-xl-6", children=[
+                html.Div(className="card", children=[
+                    html.Div(className="card-header", children=[
+                        html.H6(children='Grafico de barras de programas academicos y cantidad de matriculados'),
+                        ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='barMaxProgramsByInstitution',
+                                figure=figBarfMaPr
+                            ),
+                            ])
+                    ])
+                ]),
+            # columna para barras verticales 
+                html.Div(className="col-12 col-xl-6 pb-5", children=[
+                    html.Div(className="card", children=[
+                        html.Div(className="card-header", children=[
+                            html.H6(children='Grafico de pie de programas academicos y cantidad de matriculados'),
+                            ]),
+                        html.Div(className="card-body", children=[
+                            dcc.Graph(
+                                id='pieMaxProgramsByInstitution',
+                                figure=figPieMaPr
+                            )
+                            ])
+                        ])
+                    ])
+            ])
+        ])
+    
 ])
 
 
